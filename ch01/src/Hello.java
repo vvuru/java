ef80e155b56ae8d6064f76126810387735d0f520
vvuru <wown5398@gmail.com>
2023-09-10 23:59:26 +0900

    test

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// test commit
 		System.out.println("Hello World"); 
 		System.out.println("Hello World"); 
 		System.out.println("Hello World"); 
